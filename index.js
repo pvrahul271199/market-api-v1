f0d9f2f345208c4960a4cc4a83dcbcc4a512c574
pvrahul271199 <v-rveettil@expediagroup.com>
2025-01-22 11:18:11 +0530

    bug fix

--- index.js
+++ index.js
@@ -45,3 +45,7 @@ app.get('/', (req, res) => {
     console.log('Root endpoint hit');
     res.send('v1 Server is running');
 });
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
